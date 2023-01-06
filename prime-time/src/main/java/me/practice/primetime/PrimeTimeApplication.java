a022ffacdf62ae65da4fc093c7e8ea39625f0b67
mgcioce <mgcioce@gmail.com>
2023-01-06 17:59:37 -0500

    initial commit for prime time web service

---
+++ prime-time/src/main/java/me/practice/primetime/PrimeTimeApplication.java
@@ -0,0 +1,13 @@
+package me.practice.primetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeTimeApplication.class, args);
+	}
+
+}
---
+++ prime-time/src/test/java/me/practice/primetime/PrimeTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.practice.primetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
