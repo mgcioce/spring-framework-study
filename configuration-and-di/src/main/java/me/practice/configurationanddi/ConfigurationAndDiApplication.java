cace6e1cdced482e5ed178d7de1ccf0094a3e9ae
mgcioce <mgcioce@gmail.com>
2023-01-06 17:44:46 -0500

    adding project for practicing spring bean configuration

---
+++ configuration-and-di/src/main/java/me/practice/configurationanddi/ConfigurationAndDiApplication.java
@@ -0,0 +1,13 @@
+package me.practice.configurationanddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationAndDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationAndDiApplication.class, args);
+    }
+
+}
