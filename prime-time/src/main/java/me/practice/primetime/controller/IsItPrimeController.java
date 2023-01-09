3c2e0f5bc04e41a7117a8207be34419482320560
mgcioce <mgcioce@gmail.com>
2023-01-09 12:48:32 -0500

    pom.xml - added jackson library for xml conversion, added xml and json as request and response possibilities

--- prime-time/src/main/java/me/practice/primetime/controller/IsItPrimeController.java
+++ prime-time/src/main/java/me/practice/primetime/controller/IsItPrimeController.java
@@ -1,7 +1,9 @@
 package me.practice.primetime.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = {"prime"})
 public class IsItPrimeController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<Boolean> getPrime(){
-        return new ResponseEntity<>(Boolean.TRUE,HttpStatus.ACCEPTED);
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    @RequestMapping(method = RequestMethod.GET,consumes = {"application/json"},
+                                                produces = {"application/json","application/xml"})
+    public ResponseEntity<Boolean> getPrime(@RequestHeader(HttpHeaders.ACCEPT) String acceptHeader){
+        MediaType mediaType = null;
+        if(APPLICATION_JSON.equals(acceptHeader))
+            mediaType = MediaType.APPLICATION_JSON;
+        else if (APPLICATION_XML.equals(acceptHeader)){
+            mediaType = MediaType.APPLICATION_XML;
+        }
+
+        ResponseEntity<Boolean> response = ResponseEntity.accepted().contentType(mediaType).body(Boolean.TRUE);
+        return response;
     }
 }
